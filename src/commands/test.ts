8c2a814c8a400af27818bc7662dc819cbce773ff
Kris Nye <krisnye@gmail.com>
2019-07-28 00:17:51 -0700

    added support for not using ava

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -2,8 +2,13 @@ import * as common from "../common"
 
 //  runs unit tests
 export default function test(watch: boolean = false) {
-    if (watch)
-        return common.run("ava", ["--watch", "--verbose"])
-    else
+    let hasAva = common.getPackageJson().dependencies.ava != null
+    if (watch) {
+        if (hasAva) {
+            return common.run("ava", ["--watch", "--verbose"])
+        }
+    }
+    else {
         return common.runSync("ava", [])
+    }
 }
